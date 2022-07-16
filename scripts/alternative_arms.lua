406fae9dbd715f3f7580cee2bbac7bd37371f092
Gakuto1112 <gakuto131112@gmail.com>
2022-07-17 00:51:36 +0900

    ケーキのモデル作成

--- scripts/alternative_arms.lua
+++ scripts/alternative_arms.lua
@@ -2,10 +2,14 @@
 
 AlternativeArmsClass = {}
 
-models.models.alternative_arms:setVisible(false)
-models.models.alternative_arms.Body.Arms.RightArm:setParentType("None")
-models.models.alternative_arms.Body.Arms.RightArm.RightArmBottom:setParentType("None")
-models.models.alternative_arms.Body.Arms.LeftArm:setParentType("None")
-models.models.alternative_arms.Body.Arms.LeftArm.LeftArmBottom:setParentType("None")
+for _, modelPart in ipairs({models.models.alternative_arms, models.models.alternative_arms.Body.Arms.RightArm.RightArmBottom.RightCake, models.models.alternative_arms.Body.Arms.LeftArm.LeftArmBottom.LeftCake}) do
+	modelPart:setVisible(false)
+end
+for _, modelPart in ipairs({models.models.alternative_arms.Body.Arms.RightArm, models.models.alternative_arms.Body.Arms.RightArm.RightArmBottom, models.models.alternative_arms.Body.Arms.LeftArm, models.models.alternative_arms.Body.Arms.LeftArm.LeftArmBottom}) do
+	modelPart:setParentType("None")
+end
+for _, cakePlate in ipairs({models.models.alternative_arms.Body.Arms.RightArm.RightArmBottom.RightCake.RightCakePlate, models.models.alternative_arms.Body.Arms.LeftArm.LeftArmBottom.LeftCake.LeftCakePlate}) do
+	cakePlate:setPrimaryTexture("resource", "minecraft:textures/block/spruce_planks.png")
+end
 
 return AlternativeArmsClass
