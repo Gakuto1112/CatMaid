52ce77373f140bff797dcd2f94c6b1b6ded1bdbf
Gakuto1112 <gakuto131112@gmail.com>
2022-07-16 13:59:33 +0900

    メイドスカートを制御するクラスの追加

--- scripts/main.lua
+++ scripts/main.lua
@@ -1,10 +1,14 @@
 --クラスのインスタンス化
 General = require("scripts/general")
+--パーツ別クラス
 HairPhysicsClass = require("scripts/hair_physics")
 TailAndEarsClass = require("scripts/tail_and_ears")
 EyesAndMouthClass = require("scripts/eyes_and_mouth")
 BellSoundClass = require("scripts/bell_sound")
+SkirtClass = require("scripts/skirt")
+--機能別クラス
 MeowClass = require("scripts/meow")
 HurtClass = require("scripts/hurt")
 
+--初期化処理
 vanilla_model.PLAYER:setVisible(false)
---
+++ scripts/skirt.lua
@@ -0,0 +1,15 @@
+---@class メイドスカートを制御するクラス
+
+SkirtClass = {}
+
+events.TICK:register(function()
+	local skirt = models.models.main.Avatar.Body.Skirt
+	if string.find(General.hasItem(player:getItem(5)), "chestplate$") then --TODO: 防具非表示のオプションも考慮する。
+		skirt:setVisible(false)
+	else
+		skirt:setVisible(true)
+		skirt:setRot(General.getSneakPrevTick() and player:getPose() == "CROUCHING" and not player:isFlying() and 15 or 0, 0, 0)
+	end
+end)
+
+return SkirtClass
