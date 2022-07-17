ff5a5435d35b8adabbd25acaf9e7c852200572db
Gakuto1112 <gakuto131112@gmail.com>
2022-07-17 15:24:29 +0900

    スニークしたらまた腕の向きがおかしくなったので修正

--- scripts/arms.lua
+++ scripts/arms.lua
@@ -1,11 +1,15 @@
 ---@class 腕を制御するクラス
+---@field ArmsClass.isSneaking boolean スニークしていて、腕の補正が必要かどうかを返す。
 
 ArmsClass = {}
 
+ArmsClass.isSneaking = false
+
 events.TICK:register(function()
+	ArmsClass.isSneaking = player:isSneaking() and not player:isFlying() and player:getPose() == "CROUCHING" and not renderer:isFirstPerson()
 	local rightArm = models.models.main.Avatar.Body.Arms.RightArm
 	local leftArm = models.models.main.Avatar.Body.Arms.LeftArm
-	if player:isSneaking() and not player:isFlying() and player:getPose() == "CROUCHING" and not renderer:isFirstPerson() then
+	if ArmsClass.isSneaking then
 		for _, armPart in ipairs({rightArm, leftArm}) do
 			armPart:setPos(0, 3, 0)
 			armPart:setRot(30, 0, 0)
