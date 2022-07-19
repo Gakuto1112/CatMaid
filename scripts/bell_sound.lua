5619e60b377c4dbe1ffa9439cecc81f9d018a118
Gakuto1112 <gakuto131112@gmail.com>
2022-07-19 16:31:04 +0900

    鈴の音量を下げた

--- scripts/bell_sound.lua
+++ scripts/bell_sound.lua
@@ -15,7 +15,7 @@ OnGroundData = {}
 function BellSoundClass.playBellSound()
 	local playerPos = player:getPos()
 	if ConfigClass.BellSound then
-		local volume = WardenClass.WardenNearby and 0.025 or ((player:isSneaking() or player:isUnderwater()) and 0.05 or 0.25)
+		local volume = WardenClass.WardenNearby and 0.025 or ((player:isSneaking() or player:isUnderwater()) and 0.05 or 0.15)
 		if meta:canUseCustomSounds() then
 			sound:playSound("bell", playerPos, volume, 1)
 		else
