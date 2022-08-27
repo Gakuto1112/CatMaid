cdda1f9fe4aafc14aac4c9c4122ffb015a2286c2
Gakuto1112 <gakuto131112@gmail.com>
2022-08-28 00:10:55 +0900

    望遠鏡の基準点の設定

--- scripts/arms.lua
+++ scripts/arms.lua
@@ -22,6 +22,26 @@ events.TICK:register(function()
 	end
 end)
 
+events.RENDER:register(function ()
+	if animations["main"]["sit_down"]:getPlayState() == "PLAYING" and player:isUsingItem() then
+		local rightArm = models.models.main.Avatar.Body.Arms.RightArm
+		local leftArm = models.models.main.Avatar.Body.Arms.LeftArm
+		local activeHand = player:getActiveHand()
+		local leftHanded = player:isLeftHanded()
+		local adjustArmItemList = {"minecraft:bow", "minecraft:crossbow", "minecraft:trident", "minecraft:shield", "minecraft:spyglass"}
+		if ((activeHand == "OFF_HAND" and leftHanded) or (activeHand == "MAIN_HAND" and not leftHanded)) and General.tableFind(adjustArmItemList, General.hasItem(player:getHeldItem(leftHanded))) then
+			rightArm:setRot(-35, -10, -10)
+		else
+			rightArm:setRot(0, 0, 0)
+		end
+		if ((activeHand == "MAIN_HAND" and leftHanded) or (activeHand == "OFF_HAND" and not leftHanded)) and General.tableFind(adjustArmItemList, General.hasItem(player:getHeldItem(not leftHanded))) then
+			leftArm:setRot(-35, 10, 10)
+		else
+			leftArm:setRot(0, 0, 0)
+		end
+	end
+end)
+
 for _, modelPart in ipairs({models.models.main.Avatar.Body.Arms.RightArm.RightArmBottom, models.models.main.Avatar.Body.Arms.LeftArm.LeftArmBottom}) do
 	modelPart:setParentType("None")
 end
