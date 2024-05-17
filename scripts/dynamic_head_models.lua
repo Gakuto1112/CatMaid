20d9e44df76f72a86efb60efca1231535a1b22f4
Gakuto1112 <gakuto131112@gmail.com>
2024-05-18 03:06:14 +0900

    頭ブロックに体にかかる髪を追加

--- scripts/dynamic_head_models.lua
+++ scripts/dynamic_head_models.lua
@@ -2,28 +2,28 @@
 DynamicHeadModels = {
 	---初期化関数
 	init = function ()
+        ---モデルパーツをディープコピーする。非表示のモデルパーツはコピーしない。
+        ---@param modelPart ModelPart コピーするモデルパーツ
+        ---@return ModelPart? copiedModelPart コピーされたモデルパーツ。入力されたモデルパーツが非表示の場合はnilが返る。
+        local function copyModel(modelPart)
+            if modelPart:getVisible() then
+                local copy = modelPart:copy(modelPart:getName())
+                copy:setParentType("None")
+                copy:setVisible(true)
+                for _, child in ipairs(copy:getChildren()) do
+                    copy:removeChild(child)
+                    local childModel = copyModel(child)
+                    if childModel ~= nil then
+                        copy:addChild(childModel)
+                    end
+                end
+                return copy
+            end
+        end
+
         ---プレイヤーの頭及びポートレートモデルを生成する。
         ---@param root ModelPart 頭モデルのルートとなるモデルパーツ
         local function generateDynamicHeadModels(root)
-            ---モデルパーツをディープコピーする。非表示のモデルパーツはコピーしない。
-            ---@param modelPart ModelPart コピーするモデルパーツ
-            ---@return ModelPart? copiedModelPart コピーされたモデルパーツ。入力されたモデルパーツが非表示の場合はnilが返る。
-            local function copyModel(modelPart)
-                if modelPart:getVisible() then
-                    local copy = modelPart:copy(modelPart:getName())
-                    copy:setParentType("None")
-                    copy:setVisible(true)
-                    for _, child in ipairs(copy:getChildren()) do
-                        copy:removeChild(child)
-                        local childModel = copyModel(child)
-                        if childModel ~= nil then
-                            copy:addChild(childModel)
-                        end
-                    end
-                    return copy
-                end
-            end
-
             local copiedPart = copyModel(models.models.main.Avatar.Head)
             if copiedPart ~= nil then
                 root:addChild(copiedPart)
@@ -43,6 +43,10 @@ DynamicHeadModels = {
         for _, modelPart in ipairs({models.script_head_block, models.script_portrait}) do
             modelPart:setPos(0, -24, 0)
             generateDynamicHeadModels(modelPart)
+        end
+        local copiedPart = copyModel(models.models.main.Avatar.Torso.Body.Hairs)
+        if copiedPart ~= nil then
+            models.script_head_block.Head:addChild(copiedPart)
         end
 	end
 }
